2ccff5848189ec51bf91884b773d53db1c3e3af4
altvod <grisha100@gmail.com>
2016-02-14 02:03:09 +0300

    Moved examples up one level

--- examples/prebuilt.py
+++ examples/prebuilt.py
@@ -9,9 +9,14 @@ class SaladContext(PrebuiltCommandContext, StandardPrompt):
     def caesar_salad(self, do_what):
         self.write('{0}ing caesar salad...\n'.format(do_what.capitalize()))
 
-    @Registry.bind_argparse('buy', {'kind_of_salad': {}})
-    def buy_salad(self, kind_of_salad):
-        self.write('Buying {0} salad...\n'.format(kind_of_salad))
+    @Registry.bind_argparse('buy', {
+        'kind_of_salad': {},
+        ('--price', '-p'): {'default': None}
+    })
+    def buy_salad(self, kind_of_salad, price):
+        self.write('Buying {0} salad{1}...\n'.format(
+            kind_of_salad, ' for {0}'.format(price) if price else '')
+        )
 
 
 def main():
