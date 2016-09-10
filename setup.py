8cccf2d315b3c68ea8008009ffd6b624d1d69357
Altvod <altvod@grisha-pb>
2016-09-10 04:27:38 +0300

    Bumped the version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='pymander',
 
-    version='0.1.3',
+    version='0.2.0',
 
     description='An interactive console application library for Python',
     long_description=long_description,
